39f0916244a07a82f84df216f9002efda5deb092
James101217 <105470172+James101217@users.noreply.github.com>
2025-06-09 10:32:06 +0800

    Add files via upload

---
+++ asdf2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char letter;
+	cout << "請輸入一個英文字母 : ";
+	cin >> letter;
+	if('A' <= letter && letter <= 'Z')cout << "這是大寫英文字母 " << letter << endl;
+	else cout << "這不是大寫英文字母 " << endl;
+	
+	return 0;
+} 
